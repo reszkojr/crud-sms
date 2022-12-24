9137af1e7f60c99639c146da52bc98b0f5330956
reszkojr <s.reszkojr@gmail.com>
2022-12-23 21:14:05 -0300

    Date select

--- backend/src/main/java/com/reszkojr/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/reszkojr/dsmeta/services/SaleService.java
@@ -3,8 +3,14 @@ package com.reszkojr.dsmeta.services;
 import com.reszkojr.dsmeta.entities.Sale;
 import com.reszkojr.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
 
 @Service
@@ -13,7 +19,12 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        LocalDate now = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = (minDate.equals("") ? now.minusYears(1) : LocalDate.parse(minDate));
+        LocalDate max = (maxDate.equals("") ? now : LocalDate.parse(maxDate));
+
+        return repository.findSales(min, max, pageable);
     }
 }

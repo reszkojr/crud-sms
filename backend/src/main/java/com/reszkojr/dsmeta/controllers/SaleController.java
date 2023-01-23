51becf3990ed0d7b6a712c02f15c9b9fe2681126
reszkojr <s.reszkojr@gmail.com>
2023-01-23 19:39:25 -0300

    Twilio/SMS implementation

--- backend/src/main/java/com/reszkojr/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/reszkojr/dsmeta/controllers/SaleController.java
@@ -1,16 +1,14 @@
 package com.reszkojr.dsmeta.controllers;
 
 import com.reszkojr.dsmeta.entities.Sale;
+import com.reszkojr.dsmeta.services.SMSService;
 import com.reszkojr.dsmeta.services.SaleService;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +18,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SMSService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
